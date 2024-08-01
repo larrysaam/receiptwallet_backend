cc2fe19b8a7f6b29004a0709581cf11dec2f4c90
larrysaam <nyaah.saam@ictuniversity.edu.cm>
2024-08-01 14:43:58 +0100

    api

--- app.js
+++ app.js
@@ -36,9 +36,53 @@ app.use(cors())
 
 
 // routes
-app.use('/api/webhooks', auth)
 app.use('/user', userRoute)
 app.use('/receipt', receiptRoute)
+
+// Real code
+app.post(
+    '/api/webhooks',
+    bodyParser.raw({ type: 'application/json' }),
+    async function (req, res) {
+      try {
+        const payloadString = req.body.toString();
+        const svixHeaders = req.headers;
+
+        const wh = new Webhook('whsec_QDnDv+teoIOXIbt0Kyq8mD+7dv6WwXR4');
+        const evt = wh.verify(payloadString, svixHeaders);
+        const { id, ...attributes } = evt.data;
+        // Handle the webhooks
+        const eventType = evt.type;
+        if (eventType === 'user.created') {
+          console.log(`User ${id} was ${eventType}`);
+          console.log(attributes);
+          const email = attributes.email;
+			const phone = attributes.phoneNumbers
+    	
+			const user = new User({
+				_id: new mongoose.Types.ObjectId(),
+				email,
+				country: '',
+				plan: 'free',
+				phone
+			})
+			console.log('User saved to database');
+			await user.save()
+		
+        }
+        res.status(200).json({
+          success: true,
+          message: 'Webhook received',
+        });
+      } catch (err) {
+        res.status(400).json({
+          success: false,
+          message: err.message,
+        });
+      }
+    }
+  );
+
 
 
 
