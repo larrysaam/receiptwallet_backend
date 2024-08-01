afa255e6ca477723780f2f5beeebebb207fcb8c0
larrysaam <nyaah.saam@ictuniversity.edu.cm>
2024-08-01 16:33:25 +0100

    api

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
 const cors = require('cors')
+const dotenv =  require('dotenv')
 const userRoute = require('./routes/user')
-const auth = require('./middleware/auth')
 const receiptRoute = require('./routes/receipt')
 const express = require('express')
-const passport = require('passport')
 const bodyParser = require('body-parser')
 const cookieSession = require('cookie-session')
 const {Webhook} = require('svix')
@@ -16,6 +15,8 @@ const app = express()
 
 //connecting to mongodb atlas database (Cloud)
 mongoose.connect('mongodb+srv://Larrien:qwerty123456@cluster0.u7xnpo6.mongodb.net/ReceiptWallet?retryWrites=true&w=majority')
+
+dotenv.config();
 
 //middlewares
 app.use(bodyParser.urlencoded({extended : false}))
@@ -41,20 +42,19 @@ app.use(cors())
 app.use('/user', userRoute)
 app.use('/receipt', receiptRoute)
 
-// Real code
 app.post(
     '/api/webhooks',
     bodyParser.raw({ type: 'application/json' }),
     async function (req, res) {
-        
       try {
         const payloadString = req.body.toString();
         const svixHeaders = req.headers;
-
-        const wh = new Webhook('whsec_QDnDv+teoIOXIbt0Kyq8mD+7dv6WwXR4');
+  
+        const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET_KEY);
         const evt = wh.verify(payloadString, svixHeaders);
+  
         const { id, ...attributes } = evt.data;
-        // Handle the webhooks
+  
         const eventType = evt.type;
         if (eventType === 'user.created') {
           console.log(`User ${id} was ${eventType}`);
